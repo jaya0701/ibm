43121f88f9b0f153f399d19d353cf7cd41d4eb87
jaya0701 <114498816+jaya0701@users.noreply.github.com>
2022-09-27 20:14:14 +0530

    Add files via upload

---
+++ A.jayakumar.2.py
@@ -0,0 +1,8 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/RAJ/AppData/Local/Programs/Python/Python310/A.jayakumar.2.py
+Traceback (most recent call last):
+  File "C:/Users/RAJ/AppData/Local/Programs/Python/Python310/A.jayakumar.2.py", line 3, in <module>
+    import requests, json
+ModuleNotFoundError: No module named 'requests'
